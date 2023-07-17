cba489b854c12fa0ebcb8dbffab6d1f337b58229
Joshua Carroll <joshua.carroll@snowflake.com>
2023-07-16 19:46:55 -0700

    Clean ups on chat_with_documents and add a quick Retrieval Handler

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -5,13 +5,14 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
 from langchain.memory import ConversationBufferMemory
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.callbacks import StreamlitCallbackHandler
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 st.set_page_config(page_title="LangChain: Chat with Documents", page_icon="🦜")
 st.title("🦜 LangChain: Chat with Documents")
+
 
 @st.cache_resource
 def configure_qa_chain(uploaded_files):
@@ -20,13 +21,13 @@ def configure_qa_chain(uploaded_files):
     temp_dir = tempfile.TemporaryDirectory()
     for file in uploaded_files:
         temp_filepath = os.path.join(temp_dir.name, file.name)
-        with open(temp_filepath, 'wb') as f:
+        with open(temp_filepath, "wb") as f:
             f.write(file.getvalue())
         loader = PyPDFLoader(temp_filepath)
         docs.extend(loader.load())
 
     # Split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500,chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
 
     # Create embeddings and store in vectordb
@@ -34,15 +35,34 @@ def configure_qa_chain(uploaded_files):
     vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
 
     # Define retriever
-    retriever = vectordb.as_retriever(search_type='mmr',search_kwargs={'k':2, 'fetch_k':4})
+    retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4})
 
-    # Setup memory for contextual conversation        
-    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+    # Setup memory for contextual conversation
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     # Setup LLM and QA chain
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True)
-    qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, verbose=True)
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True
+    )
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm, retriever=retriever, memory=memory, verbose=True
+    )
     return qa_chain
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.container = container.expander("Context Retrieval")
+
+    def on_retriever_start(self, query: str, **kwargs):
+        self.container.write(f"**Question:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        # self.container.write(documents)
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.container.write(f"**Document {idx} from {source}**")
+            self.container.markdown(doc.page_content)
 
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
@@ -50,18 +70,20 @@ if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
     st.stop()
 
-uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
+uploaded_files = st.sidebar.file_uploader(
+    label="Upload PDF files", type=["pdf"], accept_multiple_files=True
+)
 if not uploaded_files:
-    st.error("Please upload PDF documents to continue.")
+    st.info("Please upload PDF documents to continue.")
     st.stop()
 
-if "messages" not in st.session_state:
+qa_chain = configure_qa_chain(uploaded_files)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-
-qa_chain = configure_qa_chain(uploaded_files)
 
 user_query = st.chat_input(placeholder="Ask me anything!")
 
@@ -70,7 +92,7 @@ if user_query:
     st.chat_message("user").write(user_query)
 
     with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container())
-        response = qa_chain.run(user_query, callbacks=[st_cb])
+        cb = PrintRetrievalHandler(st.container())
+        response = qa_chain.run(user_query, callbacks=[cb])
         st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
