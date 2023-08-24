0d2fc797a7d82426b7faea0dcc05e27725a97607
ma-raza <amjadraza24@gmail.com>
2023-08-24 14:12:10 +1000

    File Extensions Updated (#23)

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -12,27 +12,6 @@ file_formats = {
     "xlsx": pd.read_excel,
     "xlsm": pd.read_excel,
     "xlsb": pd.read_excel,
-    "json": pd.read_json,
-    "html": pd.read_html,
-    "sql": pd.read_sql,
-    "feather": pd.read_feather,
-    "parquet": pd.read_parquet,
-    "dta": pd.read_stata,
-    "sas7bdat": pd.read_sas,
-    "h5": pd.read_hdf,
-    "hdf5": pd.read_hdf,
-    "pkl": pd.read_pickle,
-    "pickle": pd.read_pickle,
-    "gbq": pd.read_gbq,
-    "orc": pd.read_orc,
-    "xpt": pd.read_sas,
-    "sav": pd.read_spss,
-    "gz": pd.read_csv,
-    "zip": pd.read_csv,
-    "bz2": pd.read_csv,
-    "xz": pd.read_csv,
-    "txt": pd.read_csv,
-    "xml": pd.read_xml,
 }
 
 
@@ -58,8 +37,8 @@ def load_data(uploaded_file):
         return None
 
 
-st.set_page_config(page_title="LangChain: Chat with Pandas DataFrame", page_icon="🦜")
-st.title("🦜 LangChain: Chat with Pandas DataFrame")
+st.set_page_config(page_title="LangChain: Chat with pandas DataFrame", page_icon="🦜")
+st.title("🦜 LangChain: Chat with pandas DataFrame")
 
 uploaded_file = st.file_uploader(
     "Upload a Data file",
