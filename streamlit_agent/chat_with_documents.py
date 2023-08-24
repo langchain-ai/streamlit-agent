13b40aa3ae5cad1ff042543d0f76a54b3ab494dd
bruno-hl <bruno@homelight.com>
2023-08-23 22:54:48 -0300

    Workaround to prevent showing the rephrased question as output

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -45,8 +45,16 @@ class StreamHandler(BaseCallbackHandler):
     def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
         self.container = container
         self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
         self.text += token
         self.container.markdown(self.text)
 
