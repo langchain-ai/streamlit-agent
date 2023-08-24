a0400b8a8e06e508585a915ad6283c9d33b15c05
Bruno Gonzalez <72821692+bruno-uy@users.noreply.github.com>
2023-08-24 01:09:47 -0300

    Fix chat_with_documents.py callbacks (#29)
    * Fix error in PrintRetrievalHandler.on_retriever_start callback
    
    * Workaround to prevent showing the rephrased question as output

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -45,8 +45,16 @@ class StreamHandler(BaseCallbackHandler):
     def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
         self.container = container
         self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
         self.text += token
         self.container.markdown(self.text)
 
@@ -55,7 +63,7 @@ class PrintRetrievalHandler(BaseCallbackHandler):
     def __init__(self, container):
         self.status = container.status("**Context Retrieval**")
 
-    def on_retriever_start(self, query: str, **kwargs):
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
         self.status.write(f"**Question:** {query}")
         self.status.update(label=f"**Context Retrieval:** {query}")
 
